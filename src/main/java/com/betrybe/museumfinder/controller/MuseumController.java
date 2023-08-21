492eb7f75462990bdd2b810396ec0acc628cdda7
Rafael <bechstedt@hotmail.com>
2023-08-21 17:42:03 -0300

    Req 5

---
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -0,0 +1,35 @@
+package com.betrybe.museumfinder.controller;
+
+import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import com.betrybe.museumfinder.util.ModelDtoConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/museums")
+public class MuseumController {
+
+  private final MuseumServiceInterface museumService;
+
+  @Autowired
+  public MuseumController(MuseumServiceInterface museumServiceInterface) {
+    this.museumService = museumServiceInterface;
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public MuseumDto createMuseum(@RequestBody MuseumCreationDto newMuseum) {
+    Museum museumConverted = ModelDtoConverter.dtoToModel(newMuseum);
+    Museum museumCreated = museumService.createMuseum(museumConverted);
+    MuseumDto museumDto = ModelDtoConverter.modelToDto(museumCreated);
+    return museumDto;
+  }
+}
